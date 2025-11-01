2c7c83adc6f29ca751253b237d75ef895b2e05b1
Sergey Kuznetsov <sergey@iterudit.com>
2025-10-31 23:39:07 -0400

    typed_nodes refactoring continues, part 3

--- crystal_gpt5/spec/parser_previous_def_spec.cr
+++ crystal_gpt5/spec/parser_previous_def_spec.cr
@@ -2,6 +2,19 @@ require "spec"
 
 require "../src/compiler/frontend/parser"
 
+private def fetch_class(program)
+  program.arena[program.roots.first].as(CrystalGPT5::Compiler::Frontend::ClassNode)
+end
+
+private def fetch_method(arena, expr_id)
+  arena[expr_id].as(CrystalGPT5::Compiler::Frontend::DefNode)
+end
+
+private def fetch_previous_def(arena, method_node)
+  body = method_node.body.not_nil!
+  arena[body.first].as(CrystalGPT5::Compiler::Frontend::PreviousDefNode)
+end
+
 describe "CrystalGPT5::Compiler::Frontend::Parser" do
   describe "Phase 96: previous_def keyword" do
     it "parses previous_def without parentheses (implicit args)" do
@@ -18,16 +31,12 @@ describe "CrystalGPT5::Compiler::Frontend::Parser" do
 
       program.roots.size.should eq(1)
       arena = program.arena
-      class_node = arena[program.roots.first]
+      class_node = fetch_class(program)
+      class_body = class_node.body.not_nil!
+      method_node = fetch_method(arena, class_body[0])
+      previous_def_node = fetch_previous_def(arena, method_node)
 
-      class_body = CrystalGPT5::Compiler::Frontend.node_class_body(class_node).not_nil!
-      method_node = arena[class_body[0]]
-
-      method_body = CrystalGPT5::Compiler::Frontend.node_def_body(method_node).not_nil!
-      previous_def_node = arena[method_body[0]]
-
-      CrystalGPT5::Compiler::Frontend.node_kind(previous_def_node).should eq(CrystalGPT5::Compiler::Frontend::NodeKind::PreviousDef)
-      CrystalGPT5::Compiler::Frontend.node_previous_def_args(previous_def_node).should be_nil  # nil = implicit args
+      previous_def_node.args.should be_nil  # nil = implicit args
     end
 
     it "parses previous_def with empty parentheses (explicit no args)" do
@@ -44,16 +53,12 @@ describe "CrystalGPT5::Compiler::Frontend::Parser" do
 
       program.roots.size.should eq(1)
       arena = program.arena
-      class_node = arena[program.roots.first]
+      class_node = fetch_class(program)
+      class_body = class_node.body.not_nil!
+      method_node = fetch_method(arena, class_body[0])
+      previous_def_node = fetch_previous_def(arena, method_node)
 
-      class_body = CrystalGPT5::Compiler::Frontend.node_class_body(class_node).not_nil!
-      method_node = arena[class_body[0]]
-
-      method_body = CrystalGPT5::Compiler::Frontend.node_def_body(method_node).not_nil!
-      previous_def_node = arena[method_body[0]]
-
-      CrystalGPT5::Compiler::Frontend.node_kind(previous_def_node).should eq(CrystalGPT5::Compiler::Frontend::NodeKind::PreviousDef)
-      args = CrystalGPT5::Compiler::Frontend.node_previous_def_args(previous_def_node).not_nil!
+      args = previous_def_node.args.not_nil!
       args.size.should eq(0)  # Empty array = explicit no args
     end
 
@@ -71,21 +76,16 @@ describe "CrystalGPT5::Compiler::Frontend::Parser" do
 
       program.roots.size.should eq(1)
       arena = program.arena
-      class_node = arena[program.roots.first]
+      class_node = fetch_class(program)
+      class_body = class_node.body.not_nil!
+      method_node = fetch_method(arena, class_body[0])
+      previous_def_node = fetch_previous_def(arena, method_node)
 
-      class_body = CrystalGPT5::Compiler::Frontend.node_class_body(class_node).not_nil!
-      method_node = arena[class_body[0]]
-
-      method_body = CrystalGPT5::Compiler::Frontend.node_def_body(method_node).not_nil!
-      previous_def_node = arena[method_body[0]]
-
-      CrystalGPT5::Compiler::Frontend.node_kind(previous_def_node).should eq(CrystalGPT5::Compiler::Frontend::NodeKind::PreviousDef)
-      args = CrystalGPT5::Compiler::Frontend.node_previous_def_args(previous_def_node).not_nil!
+      args = previous_def_node.args.not_nil!
       args.size.should eq(1)
 
       # Check argument is a binary expression (x + 1)
-      arg_node = arena[args[0]]
-      CrystalGPT5::Compiler::Frontend.node_kind(arg_node).should eq(CrystalGPT5::Compiler::Frontend::NodeKind::Binary)
+      arena[args[0]].as(CrystalGPT5::Compiler::Frontend::BinaryNode)
     end
 
     it "parses previous_def with multiple arguments" do
@@ -102,16 +102,12 @@ describe "CrystalGPT5::Compiler::Frontend::Parser" do
 
       program.roots.size.should eq(1)
       arena = program.arena
-      class_node = arena[program.roots.first]
+      class_node = fetch_class(program)
+      class_body = class_node.body.not_nil!
+      method_node = fetch_method(arena, class_body[0])
+      previous_def_node = fetch_previous_def(arena, method_node)
 
-      class_body = CrystalGPT5::Compiler::Frontend.node_class_body(class_node).not_nil!
-      method_node = arena[class_body[0]]
-
-      method_body = CrystalGPT5::Compiler::Frontend.node_def_body(method_node).not_nil!
-      previous_def_node = arena[method_body[0]]
-
-      CrystalGPT5::Compiler::Frontend.node_kind(previous_def_node).should eq(CrystalGPT5::Compiler::Frontend::NodeKind::PreviousDef)
-      args = CrystalGPT5::Compiler::Frontend.node_previous_def_args(previous_def_node).not_nil!
+      args = previous_def_node.args.not_nil!
       args.size.should eq(2)
     end
 
@@ -129,21 +125,16 @@ describe "CrystalGPT5::Compiler::Frontend::Parser" do
 
       program.roots.size.should eq(1)
       arena = program.arena
-      class_node = arena[program.roots.first]
+      class_node = fetch_class(program)
+      class_body = class_node.body.not_nil!
+      method_node = fetch_method(arena, class_body[0])
 
-      class_body = CrystalGPT5::Compiler::Frontend.node_class_body(class_node).not_nil!
-      method_node = arena[class_body[0]]
-
-      method_body = CrystalGPT5::Compiler::Frontend.node_def_body(method_node).not_nil!
-      if_node = arena[method_body[0]]
-
-      # Should be an If node (postfix if)
-      CrystalGPT5::Compiler::Frontend.node_kind(if_node).should eq(CrystalGPT5::Compiler::Frontend::NodeKind::If)
+      method_body = method_node.body.not_nil!
+      if_node = arena[method_body[0]].as(CrystalGPT5::Compiler::Frontend::IfNode)
 
       # Then branch should contain previous_def
-      if_then = CrystalGPT5::Compiler::Frontend.node_if_then(if_node).not_nil!
-      previous_def_node = arena[if_then[0]]
-      CrystalGPT5::Compiler::Frontend.node_kind(previous_def_node).should eq(CrystalGPT5::Compiler::Frontend::NodeKind::PreviousDef)
+      if_then = if_node.then_body
+      arena[if_then.first].as(CrystalGPT5::Compiler::Frontend::PreviousDefNode)
     end
 
     it "parses previous_def in multiple methods" do
@@ -164,22 +155,20 @@ describe "CrystalGPT5::Compiler::Frontend::Parser" do
 
       program.roots.size.should eq(1)
       arena = program.arena
-      class_node = arena[program.roots.first]
+      class_node = fetch_class(program)
 
-      class_body = CrystalGPT5::Compiler::Frontend.node_class_body(class_node).not_nil!
+      class_body = class_node.body.not_nil!
       class_body.size.should eq(2)
 
       # First method: previous_def without args
-      method1 = arena[class_body[0]]
-      body1 = CrystalGPT5::Compiler::Frontend.node_def_body(method1).not_nil!
-      previous_def1 = arena[body1[0]]
-      CrystalGPT5::Compiler::Frontend.node_previous_def_args(previous_def1).should be_nil
+      method1 = fetch_method(arena, class_body[0])
+      previous_def1 = fetch_previous_def(arena, method1)
+      previous_def1.args.should be_nil
 
       # Second method: previous_def with args
-      method2 = arena[class_body[1]]
-      body2 = CrystalGPT5::Compiler::Frontend.node_def_body(method2).not_nil!
-      previous_def2 = arena[body2[0]]
-      args = CrystalGPT5::Compiler::Frontend.node_previous_def_args(previous_def2).not_nil!
+      method2 = fetch_method(arena, class_body[1])
+      previous_def2 = fetch_previous_def(arena, method2)
+      args = previous_def2.args.not_nil!
       args.size.should eq(1)
     end
 
@@ -198,17 +187,15 @@ describe "CrystalGPT5::Compiler::Frontend::Parser" do
 
       program.roots.size.should eq(1)
       arena = program.arena
-      class_node = arena[program.roots.first]
+      class_node = fetch_class(program)
+      class_body = class_node.body.not_nil!
+      method_node = fetch_method(arena, class_body[0])
 
-      class_body = CrystalGPT5::Compiler::Frontend.node_class_body(class_node).not_nil!
-      method_node = arena[class_body[0]]
-
-      method_body = CrystalGPT5::Compiler::Frontend.node_def_body(method_node).not_nil!
+      method_body = method_node.body.not_nil!
       method_body.size.should be >= 2
 
       # First statement is previous_def
-      previous_def_node = arena[method_body[0]]
-      CrystalGPT5::Compiler::Frontend.node_kind(previous_def_node).should eq(CrystalGPT5::Compiler::Frontend::NodeKind::PreviousDef)
+      arena[method_body[0]].as(CrystalGPT5::Compiler::Frontend::PreviousDefNode)
 
       # Verify there are other statements
       method_body.size.should be > 1
@@ -228,25 +215,19 @@ describe "CrystalGPT5::Compiler::Frontend::Parser" do
 
       program.roots.size.should eq(1)
       arena = program.arena
-      class_node = arena[program.roots.first]
+      class_node = fetch_class(program)
+      class_body = class_node.body.not_nil!
+      method_node = fetch_method(arena, class_body[0])
+      previous_def_node = fetch_previous_def(arena, method_node)
 
-      class_body = CrystalGPT5::Compiler::Frontend.node_class_body(class_node).not_nil!
-      method_node = arena[class_body[0]]
-
-      method_body = CrystalGPT5::Compiler::Frontend.node_def_body(method_node).not_nil!
-      previous_def_node = arena[method_body[0]]
-
-      CrystalGPT5::Compiler::Frontend.node_kind(previous_def_node).should eq(CrystalGPT5::Compiler::Frontend::NodeKind::PreviousDef)
-      args = CrystalGPT5::Compiler::Frontend.node_previous_def_args(previous_def_node).not_nil!
+      args = previous_def_node.args.not_nil!
       args.size.should eq(2)
 
       # First arg is binary expression
-      arg1 = arena[args[0]]
-      CrystalGPT5::Compiler::Frontend.node_kind(arg1).should eq(CrystalGPT5::Compiler::Frontend::NodeKind::Binary)
+      arena[args[0]].as(CrystalGPT5::Compiler::Frontend::BinaryNode)
 
       # Second arg is ternary expression
-      arg2 = arena[args[1]]
-      CrystalGPT5::Compiler::Frontend.node_kind(arg2).should eq(CrystalGPT5::Compiler::Frontend::NodeKind::Ternary)
+      arena[args[1]].as(CrystalGPT5::Compiler::Frontend::TernaryNode)
     end
 
     it "distinguishes previous_def(), previous_def and previous_def(args)" do
@@ -271,29 +252,26 @@ describe "CrystalGPT5::Compiler::Frontend::Parser" do
 
       program.roots.size.should eq(1)
       arena = program.arena
-      class_node = arena[program.roots.first]
+      class_node = fetch_class(program)
 
-      class_body = CrystalGPT5::Compiler::Frontend.node_class_body(class_node).not_nil!
+      class_body = class_node.body.not_nil!
       class_body.size.should eq(3)
 
       # Method bar: previous_def (nil = implicit args)
-      method_bar = arena[class_body[0]]
-      body_bar = CrystalGPT5::Compiler::Frontend.node_def_body(method_bar).not_nil!
-      previous_def_bar = arena[body_bar[0]]
-      CrystalGPT5::Compiler::Frontend.node_previous_def_args(previous_def_bar).should be_nil
+      method_bar = fetch_method(arena, class_body[0])
+      previous_def_bar = fetch_previous_def(arena, method_bar)
+      previous_def_bar.args.should be_nil
 
       # Method baz: previous_def() (empty array = explicit no args)
-      method_baz = arena[class_body[1]]
-      body_baz = CrystalGPT5::Compiler::Frontend.node_def_body(method_baz).not_nil!
-      previous_def_baz = arena[body_baz[0]]
-      args_baz = CrystalGPT5::Compiler::Frontend.node_previous_def_args(previous_def_baz).not_nil!
+      method_baz = fetch_method(arena, class_body[1])
+      previous_def_baz = fetch_previous_def(arena, method_baz)
+      args_baz = previous_def_baz.args.not_nil!
       args_baz.size.should eq(0)
 
       # Method qux: previous_def(x) (array with args)
-      method_qux = arena[class_body[2]]
-      body_qux = CrystalGPT5::Compiler::Frontend.node_def_body(method_qux).not_nil!
-      previous_def_qux = arena[body_qux[0]]
-      args_qux = CrystalGPT5::Compiler::Frontend.node_previous_def_args(previous_def_qux).not_nil!
+      method_qux = fetch_method(arena, class_body[2])
+      previous_def_qux = fetch_previous_def(arena, method_qux)
+      args_qux = previous_def_qux.args.not_nil!
       args_qux.size.should eq(1)
     end
   end
